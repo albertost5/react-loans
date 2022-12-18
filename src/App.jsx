6554ce5f6e10b847917a0b4308ba275ccc3b35f7
albertost5 <sancheztorresalberto@gmail.com>
2022-12-18 14:53:19 +0100

    Add fn total to pay, useEffect and loans summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import { formatMoney } from "./helpers";
+import { formatMoney, calculateTotalToPay } from "./helpers";
 
 function App() {
 
@@ -10,6 +10,12 @@ function App() {
   const STEP = 100;
   
   const [quantity, setQuantity] = useState(MAX/2);
+  const [months, setMonths] = useState(6);
+  const [total, setTotal] = useState(0);
+
+  useEffect(()=> {
+    setTotal(calculateTotalToPay(quantity, months));
+  }, [quantity, months]);
   
   function handleChange(e) {
     setQuantity(+e.target.value);
@@ -38,7 +44,7 @@ function App() {
   }
 
   return (
-    <div className="my-20 mx-w-lg mx-auto bg-white shadow p-10">
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
       <div className="flex justify-between my-6">
         <Button 
@@ -63,6 +69,30 @@ function App() {
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatMoney(quantity)}  
       </p>
+      
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Choose a <span className="text-indigo-600">month</span> to pay
+      </h2>
+
+      <select 
+        className="mt-5 w-full p-2 bg-white border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={months}
+        onChange={ e => setMonths(+e.target.value) }
+      >
+        <option value="6">6 Months</option>
+        <option value="12">12 Months</option>
+        <option value="24">24 Months</option>
+      </select>
+
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center uppercase">
+          Total <span className="text-indigo-600">to pay</span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold">{months} Months</p>
+        <p className="text-xl text-gray-500 text-center font-bold">{formatMoney(total)} Total </p>
+        <p className="text-xl text-gray-500 text-center font-bold">{formatMoney(total/months)} / month</p>
+      </div>
+
     </div>
   )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,6 +8,31 @@ const formatMoney = (value) => {
     return formatter.format(value);
 }
 
+const calculateTotalToPay = (quantity, months) => {
+    let total;
+
+    if(quantity < 5000) {
+        total = quantity * 1.5;
+    } else if(quantity >= 5000 && quantity < 10000) {
+        total = quantity * 1.4;
+    } else if(quantity >= 10000 && quantity < 15000) {
+        total = quantity * 1.3;
+    } else {
+        total = quantity * 1.2;
+    }
+
+    if(months === 6) {
+        total *= 1.1;
+    } else if (months === 12) {
+        total *= 1.2;
+    } else {
+        total *= 1.3;
+    }
+
+    return total;
+}
+
 export {
-    formatMoney
+    formatMoney,
+    calculateTotalToPay
 }
